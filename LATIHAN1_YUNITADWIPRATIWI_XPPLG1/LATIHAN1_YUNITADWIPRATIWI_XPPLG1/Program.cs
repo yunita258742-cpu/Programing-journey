c8d6a4d0f2c43ac8b29951e0cb42e3697a237eab
yunita258742-cpu <yunita258742@student.smkn1kandeman.sch.id>
2025-10-13 13:48:33 +0700

    Add files via upload

---
+++ LATIHAN1_YUNITADWIPRATIWI_XPPLG1/LATIHAN1_YUNITADWIPRATIWI_XPPLG1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LATIHAN1_YUNITADWIPRATIWI_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program Menghitung Luas & Volume ===");
+            Console.WriteLine("1. Luas Permukaan Kubus");
+            Console.WriteLine("2. Volume Kubus");
+            Console.WriteLine("3. Luas Permukaan Balok");
+            Console.WriteLine("4. Volume Balok");
+            int pilihan = int.Parse(Console.ReadLine());
+
+            switch (pilihan)
+            {
+                case 1:
+                    Console.Write("Masukan panjang sisi kubus: ");
+                    double sisi1 = double.Parse(Console.ReadLine());
+                    double luaskubus = 6 * sisi1 * sisi1;
+                    Console.WriteLine($"Luas Permukaan Kubus = {luaskubus}"); break;
+
+                case 2:
+                    Console.Write("Masukan panjang sisi kubus: ");
+                    double sisi2 = double.Parse(Console.ReadLine());
+                    double volumekubus = sisi2 * sisi2 * sisi2;
+                    Console.WriteLine($"Volume kubus = {volumekubus}"); break;
+
+                case 3:
+                    Console.Write("Masukan panjang balok: ");
+                    double p = double.Parse(Console.ReadLine());
+                    Console.Write("Masukan lebar balok: ");
+                    double l = double.Parse(Console.ReadLine());
+                    Console.Write("Masukan tinggi balok: ");
+                    double t = double.Parse(Console.ReadLine());
+                    double luasbalok = 2 * ((p * l) + (p * t) + (l * t));
+                    Console.WriteLine($"Luas permukaan balok = {luasbalok}"); break;
+
+                case 4:
+                    Console.Write("Masukan panjang balok: ");
+                    double pb = double.Parse(Console.ReadLine());
+                    Console.Write("Masukan lebar balok: ");
+                    double lb = double.Parse(Console.ReadLine());
+                    Console.Write("Masukan tinggi balok: ");
+                    double tb = double.Parse(Console.ReadLine());
+                    double volumebalok = pb * lb * tb;
+                    Console.WriteLine($"Volume balok = {volumebalok}"); break;
+
+                default:
+                    Console.WriteLine("Pilihan tidak tersedia!"); break;
+            }
+
+            Console.WriteLine("\nTerima kasih telah menggunakan program ini!");
+        }
+    }
+}
+        
+    
+
