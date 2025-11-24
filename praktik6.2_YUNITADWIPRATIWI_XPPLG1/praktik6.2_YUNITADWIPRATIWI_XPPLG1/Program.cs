9766a6291e3696188021cac8973f60036da6656b
yunita258742-cpu <yunita258742@student.smkn1kandeman.sch.id>
2025-11-24 08:30:44 +0700

    Add files via upload

---
+++ praktik6.2_YUNITADWIPRATIWI_XPPLG1/praktik6.2_YUNITADWIPRATIWI_XPPLG1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._2_YUNITADWIPRATIWI_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           c
+        }
+    }
+}
