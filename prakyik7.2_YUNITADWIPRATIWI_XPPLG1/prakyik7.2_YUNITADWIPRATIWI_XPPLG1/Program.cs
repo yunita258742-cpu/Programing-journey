1a86fe15c78bf445d1989af60b3043df4142676d
yunita258742-cpu <yunita258742@student.smkn1kandeman.sch.id>
2025-11-24 10:36:04 +0700

    Add files via upload

---
+++ prakyik7.2_YUNITADWIPRATIWI_XPPLG1/prakyik7.2_YUNITADWIPRATIWI_XPPLG1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakyik7._2_YUNITADWIPRATIWI_XPPLG1
+{
+    internal class Program
+    {
+            static void Sapanama(string nama)
+            {
+                Console.WriteLine("Hallo, " + nama + "!");
+                Console.WriteLine("Semengat belajar C# hari ini!");
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("Masukkan nama anda: ");
+                string namaSiswa = Console.ReadLine();
+                Sapanama(namaSiswa); // Memanggil fungsi dengan argumen
+            }
+    }
+}
